09f9c0574339b5277bc8e5fdbed0ce7e0bf3d4f1
David Elster <davidandtheweb@gmail.com>
2018-11-07 21:35:46 -0500

    014 ES6 Aside Arrow Functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,22 +1,7 @@
 'use strict';
 
-var nameVar = 'Andrew';
-var nameVar = 'Mike';
-console.log('nameVar', nameVar);
+var getFirstName = function getFirstName(fullName) {
+  return fullName.split(' ')[0];
+};
 
-var nameLet = 'Jen';
-nameLet = 'Julie';
-console.log('nameLet', nameLet);
-
-var nameConst = 'Frank';
-console.log('nameConst', nameConst);
-
-var fullName = 'Jen Mead';
-var firstName = void 0;
-
-if (fullName) {
-  firstName = fullName.split(' ')[0];
-  console.log(firstName);
-}
-
-console.log(firstName);
+console.log(getFirstName('Andrew Mead'));
